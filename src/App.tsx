4813fbcddd99361a23c12ed0c2bb376174b05076
Romaniuk Oksana <rom.feshchenko@gmail.com>
2022-05-04 16:11:24 +0200

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import React from "react";
+//import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => (
+  <div className="App">
+    <h1>Привіт!</h1>
+    <h2>Мене звати Роман</h2>
+    <p>В дитинстві я ходив у дитячий садочок, потім у школу, а потім в університет.</p>
+    <a href="https://www.linkedin.com/in/roman-feshchenko-15a240138/" target="_blank">Це лінк на мій профіль</a>
+    <h2>На лінк можна спокійно натискати, він відчиниться в новій вкладці</h2>
+  </div>
+);
 
 export default App;
